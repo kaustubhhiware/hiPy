c262b3bd74976d87753b2a7c70bf13479ac1762b
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-06-27 17:52:27 +0530

    Rotation code corrected for encoding and decoding

--- think_python/palindrome_imp.py
+++ think_python/palindrome_imp.py
@@ -6,3 +6,13 @@ else:
 	print 'you are weak now , return later'
 
 #improved version based on strings
+"""
+Something interesting : Collatz conjecture
+
+Consider the following operation on an arbitrary positive integer:
+
+    If the number is even, divide it by two.
+    If the number is odd, triple it and add one.
+
+    Our main man said that this eventually stops at 1
+"""
