db43b2fe5c713ea91e189fb464d479142c5b7134
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-07-01 03:02:17 +0530

    tuple begins

---
+++ think_python/tuples.py
@@ -0,0 +1,70 @@
+
+def min_max(t):
+	return min(t),max(t)
+
+sum = 0
+def sumall(*args):
+	"""sum(1,2,3) gives invalid o/p
+	"""
+	global sum
+	for i in args:
+		sum += i
+	return sum
+
+
+print 'Tuples are a seq of vals , indexed by integers ~ list'
+
+t1 = 'a',
+print 't1 = \'a\',',type(t1)
+print 'The comma is important\n'
+
+t2 = ('a')
+print 't2 = (\'a\')',type(t2)
+
+t = tuple()
+print t
+
+t = tuple('lupins')
+print t
+
+print '\nlist operations work on tuples as well like'
+print 't[0] = ',t[0]
+print 't[1:3] = ',t[1:3]
+
+print '\nHowever , t[0] = \'A\' gives a type error .\nYou could just do t = (\'A\',)+t[1:]'
+
+print '\n to swap a and b , normally swap var is needed.Not here :P .Just do a,b = b,a'
+a = 10
+b = 5
+print 'a = ',a,'b = ',b
+a,b = b,a
+print 'a = ',a,'b = ',b
+
+print 'Assigning is so easy! a,b = 7,11'
+a,b = 7,11
+print a,b
+
+print '\nSplitting email address much easy'
+addr = 'name@domain.com'
+uname,domain = addr.split('@')
+
+print addr,' : ',uname,' and ',domain
+
+print '\nReturning tuples : '
+t = divmod(7,3)
+print 'divmod(7,3) = ',t
+
+numt = (1,-2,43,4)
+kam,jyada = min_max(numt)
+
+print kam,'to ',jyada
+
+print '\nTuple contains multiple type of args : use *'
+print 'def func(*args):print args'
+
+t = (7,3)
+print 't = (7,3)'
+print 'divmod(t) returns argument insufficient'
+print 'do divmod(*t) : ',divmod(*t)
+
+print 'sum tuple function for ',numt,' : ',sumall(*numt)
