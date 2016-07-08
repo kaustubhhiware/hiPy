17e20ef560b8d4fca8c64cdd387e6d64f2b90540
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-07-09 00:58:01 +0530

    Update incomplete tuple methods

--- think_python/tuples.py
+++ think_python/tuples.py
@@ -1,4 +1,4 @@
-
+from time import sleep
 def min_max(t):
 	return min(t),max(t)
 
@@ -21,16 +21,18 @@ print 'The comma is important\n'
 t2 = ('a')
 print 't2 = (\'a\')',type(t2)
 
+sleep(1)
 t = tuple()
 print t
 
 t = tuple('lupins')
 print t
+sleep(1)
 
 print '\nlist operations work on tuples as well like'
 print 't[0] = ',t[0]
 print 't[1:3] = ',t[1:3]
-
+sleep(1)
 print '\nHowever , t[0] = \'A\' gives a type error .\nYou could just do t = (\'A\',)+t[1:]'
 
 print '\n to swap a and b , normally swap var is needed.Not here :P .Just do a,b = b,a'
@@ -39,7 +41,7 @@ b = 5
 print 'a = ',a,'b = ',b
 a,b = b,a
 print 'a = ',a,'b = ',b
-
+sleep(1)
 print 'Assigning is so easy! a,b = 7,11'
 a,b = 7,11
 print a,b
@@ -49,7 +51,7 @@ addr = 'name@domain.com'
 uname,domain = addr.split('@')
 
 print addr,' : ',uname,' and ',domain
-
+sleep(1)
 print '\nReturning tuples : '
 t = divmod(7,3)
 print 'divmod(7,3) = ',t
@@ -58,7 +60,7 @@ numt = (1,-2,43,4)
 kam,jyada = min_max(numt)
 
 print kam,'to ',jyada
-
+sleep(1)
 print '\nTuple contains multiple type of args : use *'
 print 'def func(*args):print args'
 
--- think_python/tuples2.py
+++ think_python/tuples2.py
@@ -1,3 +1,4 @@
+from time import sleep
 def has_match(str1,str2):
 	tup1 = tuple(str1)
 	tup2 = tuple(str2)
@@ -23,6 +24,20 @@ def enumerated(str):
 		print index,element
 
 
+def sort_length(words):
+	t = []
+	for word in words:
+		t.append((len(word),word))#build tuple
+
+	t.sort(reverse=True)
+
+	res = []
+	for length,word in t:
+		res.append(word)
+
+	return res
+
+
 print '\nzip combines two lists into a tuple with key as first , val as second : '
 s = 'abc'
 t = [0,1,2]
@@ -40,6 +55,32 @@ print 'Check if two strings have the same letter at the same position :'
 str1 = raw_input("string 1 : ")
 str2 = raw_input("string 2 : ")
 has_match(str1,str2)
-
+sleep(1)
 print 'enumerating your string 1 :',
 enumerated(str1)
+sleep(1)
+
+print '\nconvert dict to tuple'
+d = {'a':'0','b':'1','c':'2'}
+t = d.items()
+sleep(1)
+print t
+
+print '\nWe can use tuples as key in dictionary - like last name , first name for phone dir'
+print 'directory[last,first] = number'
+
+print '\nComparing tuples : first element compared - if one is greater ,than touple is greater',
+print '\nelse it moves to the next element'
+
+print '(0,1,2) < (0,3,4) : ',(0,1,2) < (0,3,4)
+print '(0,1,2000) < (1,2,3):',(0,1,2000) < (1,2,3)
+sleep(1)
+print '\n\nTo sort by length'
+length_tup = ['ar','kar','chamatkar','chaarmindar','kirloskar','murtikar','aavishkar']
+print length_tup
+print '\nalphabetic order: '
+print sorted(length_tup)#sorts anything
+#there is also reverse like this
+print '\n'
+sorted_tup = sort_length(length_tup)
+print sorted_tup
